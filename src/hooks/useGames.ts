2973d3442e074ee282e9e9b00bfe9c6099ec9187
KlydeKun <kylelerio15@gmail.com>
2024-06-05 17:42:38 +0800

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,24 +24,30 @@ interface FetchGamesReponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]); //Storing Game objects
   const [error, setError] = useState(""); // Error Messages
+  const [isLoading, setLoading] = useState(false); // For loading skeleton
 
   // to send the fetch request to the backend
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     apiClient
       .get<FetchGamesReponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
   // To use it to other components
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
